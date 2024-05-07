0ea9dbef17105c36bf3fc77c8ede0acf27f6e247
Samuel Silva <samuelslv7@gmail.com>
2024-05-07 16:23:48 -0300

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -6,13 +6,14 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Cliente> clientes = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int opcao;
+
+        int opcao = 0;
 
         do {
-            // Exibe o menu principal usando a classe Menu
-            Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Realizar Operações", "Sair"));
-            opcao = mainMenu.getSelection();
 
+            // Exibe o menu principal usando a classe Menu
+            Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Realizar Operações", "Mostrar clientes", "Sair"));
+            opcao = mainMenu.getSelection();
             switch (opcao) {
                 case 1:
                     // Operações relacionadas a Conta
@@ -32,7 +33,9 @@ public class Main {
                         System.out.println("Conta já existente para este cliente.");
                         // Lógica para criar ou gerenciar contas existentes
                     } else {
-                        System.out.print("Cliente não encontrado. Deseja criar um novo cliente para criar a conta? (s/n): ");
+
+                        System.out
+                                .print("Cliente não encontrado. Deseja criar um novo cliente para criar a conta? (s/n): ");
                         String criarNovoCliente = scanner.next();
                         scanner.nextLine(); // Consome a quebra de linha
 
@@ -52,6 +55,7 @@ public class Main {
 
                 case 2:
                     // Menu Cliente usando MenuCliente
+
                     MenuCliente menuCliente = new MenuCliente("Menu Cliente", Arrays.asList("Criar cliente", "Excluir cliente"));
                     int escolhaCliente = menuCliente.getSelection();
 
@@ -82,4 +86,27 @@ public class Main {
                     } else if (escolhaOperacao == 2) {
                         System.out.println("Saque realizado.");
                         // Adicionar lógica de saque aqui
-         
+
+                    }
+                    break;
+
+                case 4:
+                    int i = 1;
+                    for (Cliente cliente1 : clientes) {
+                        System.out.println("\nCliente #" + i++);
+                        System.out.println(cliente1.getNome());
+                        System.out.println(cliente1.getCpf());
+                        System.out.println(cliente1.getGenero());
+                    }
+
+					break;
+                case 5:
+					System.out.println("SAIR");
+
+					break;
+
+            }
+        } while (opcao != 5);
+
+    }
+}
