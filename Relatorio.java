cea27b1d1d4f74d22d0833c373a129ec32e5dd1b
Bianca Bastos <biancabsouza23@gmail.com>
2024-05-09 00:06:06 -0300

    correcao de relatorio de saque #6

--- Relatorio.java
+++ Relatorio.java
@@ -12,31 +12,4 @@ public class Relatorio {
         }
     }
 
-    public static void RelatorioDeDepositoCliente(Cliente cl) {
-        ArrayList<Transacao> transacoes = cl.getTransacoes();
-        System.out.println("Relatório de Depósitos do Cliente: " + cl.getNome());
-        for (Transacao t : transacoes) {
-            if (t.getTipo() == 2) {
-                System.out.println("Valor: " + t.getValor());
-            }
-        }              
-    }
-
-    public static void RelatorioGeral(Cliente cl){
-        ArrayList<Transacao> transacoes = cl.getTransacoes();
-        System.out.println("Relatório Geral do Cliente: " + cl.getNome());
-        for (Transacao t : transacoes) {
-            if (t.getTipo() == 1) {
-                System.out.println("Saque: " + t.getValor());
-            } else if (t.getTipo() == 2){
-                System.out.println("Depósito: " + t.getValor());
-            } else {
-                System.out.println("Tipo de transação inválido");
-            }
-        }
-    }
-
-
-
-
 }
