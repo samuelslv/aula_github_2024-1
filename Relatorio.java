82d9e90a906faa1cb60dbec64979a162c3ee1e20
Bianca Bastos <biancabsouza23@gmail.com>
2024-05-09 00:21:54 -0300

    criacao relatorio geral #8

--- Relatorio.java
+++ Relatorio.java
@@ -22,4 +22,19 @@ public class Relatorio {
         }              
     }
 
+    public static void RelatorioGeral(Cliente cl){
+        ArrayList<Transacao> transacoes = cl.getTransacoes();
+        System.out.println("Relatório Geral do Cliente: " + cl.getNome());
+        for (Transacao t : transacoes) {
+            if (t.getTipo() == 1) {
+                System.out.println("Saque: " + t.getValor());
+            } else if (t.getTipo() == 2){
+                System.out.println("Depósito: " + t.getValor());
+            } else {
+                System.out.println("Tipo de transação inválido");
+            }
+        }
+    }
+
+
 }
